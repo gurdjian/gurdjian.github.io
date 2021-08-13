9b485e4737e730432cc630fc8d20478baab8fca4
Eduard <eduard.gurdjian@gmail.com>
2021-08-13 14:09:17 +0300

    +arenda

--- arenda/script.js
+++ arenda/script.js
@@ -5,19 +5,34 @@ const sum = document.querySelector('#sum');
 const komRange = document.querySelector('#komissiarange');
 const annRange = document.querySelector('#annualpayrange');
 
-function calcKomis() {
+function calcKomisFromRange() {
   percent.value = this.value;
   komissia.value = Math.round(this.value * annual.value * 0.01);
   sum.value = annual.value * 2 + +komissia.value;
 }
-function calcAnnual() {
-  percent.value = komRange.value;
+function calcAnnualFromRange() {
   annual.value = this.value;
   komissia.value = Math.round(this.value * percent.value * 0.01);
   sum.value = annual.value * 2 + +komissia.value;
 }
 
-komRange.addEventListener('input', calcKomis);
-komRange.addEventListener('change', calcKomis);
-annRange.addEventListener('input', calcAnnual);
-annRange.addEventListener('change', calcAnnual);
+function calcAnnualFromNum() {
+  annRange.value = this.value;
+  komissia.value = Math.round(this.value * percent.value * 0.01);
+  sum.value = annual.value * 2 + +komissia.value;
+}
+
+function calcKomisFromNum() {
+  komRange.value = this.value;
+  komissia.value = Math.round(this.value * annual.value * 0.01);
+  sum.value = annual.value * 2 + +komissia.value;
+}
+komRange.addEventListener('input', calcKomisFromRange);
+komRange.addEventListener('change', calcKomisFromRange);
+percent.addEventListener('input', calcKomisFromNum);
+percent.addEventListener('change', calcKomisFromNum);
+
+annRange.addEventListener('input', calcAnnualFromRange);
+annRange.addEventListener('change', calcAnnualFromRange);
+annual.addEventListener('input', calcAnnualFromNum);
+annual.addEventListener('change', calcAnnualFromNum);
